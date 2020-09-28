316116768f60630ce8e1f155678f84f9cfa75b0a
ELATTARIYassine <yassineattari64@gmail.com>
2020-09-28 17:15:14 +0000

    get it back in the home component

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Home Component</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Home Component</div>
 
-                    <div class="card-body">
-                        I'm an Home component.
-                    </div>
-                </div>
-            </div>
+          <div class="card-body">I'm an Home component.</div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    console.log("get it back");
+  },
+};
 </script>
